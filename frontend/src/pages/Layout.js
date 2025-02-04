8e785ecd537b44e81e1449bdf0114f5be9d3499e
Peter Kizek <peter.kizek@student.tuke.sk>
2025-02-04 16:15:28 +0100

    bootstrap, navbar, ikony

--- frontend/src/pages/Layout.js
+++ frontend/src/pages/Layout.js
@@ -1,13 +1,33 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+
+import { FaShoppingCart } from "react-icons/fa";
+
 const Layout = () => {
     return (
         <div>
-            <ul>
-                <li><Link to="/">Home</Link></li>
-                <li><Link to="/contact">Contact</Link></li>
-            </ul>
+            <Navbar expand="lg" className="bg-body-tertiary">
+            <Container fluid>
+                <Navbar.Brand as={Link} to="/">Home</Navbar.Brand>
+                <Navbar.Toggle aria-controls="navbarScroll" />
+                <Navbar.Collapse id="navbarScroll">
+                <Nav
+                    className="me-auto my-2 my-lg-0"
+                    style={{ maxHeight: '100px' }}
+                    navbarScroll>
+                    
+                    <Nav.Link as={Link} to="/contact">Contact</Nav.Link>
+                    <Nav.Link href="#" disabled>Link </Nav.Link>
+                </Nav> 
+                <Button variant="outline-success"><FaShoppingCart className="fs-2"/></Button>
+                </Navbar.Collapse>
+            </Container>
+            </Navbar>
         </div>
     );
 }
